fc89ff8248ef1cf7e56f2ddb3c76fab6f6d62966
C-Carroll <crcarroll00@gmail.com>
2023-01-31 19:21:18 -0600

    phase 1-2

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -10,16 +10,43 @@ class TreeNode {
 class BinarySearchTree {
 
   constructor() {
-    // Your code here
+    this.root = null
   }
 
   insert(val, currentNode=this.root) {
-    // Your code here
+    if (!this.root){
+      this.root = new TreeNode(val)
+    } else if (val < currentNode.val){
+      if (!currentNode.left){
+        currentNode.left = new TreeNode(val)
+      } else {
+        this.insert(val, currentNode.left)
+      }
+    } else {
+      if(!currentNode.right){
+       currentNode.right = new TreeNode(val)
+      } else {
+        this.insert(val, currentNode.right)
+      }
+    }
   }
 
-  search(val) {
-    // Your code here
+  search(val, currentNode = this.root) {
+    if (!currentNode) {
+      return false;
+    } else if (currentNode.val === val) {
+      return true;
+    // } else if (this.search(currentNode.left, val)){
+    //  return true;
+    // } else
+    // return this.search(currentNode.right, val)
+    }else if (val < currentNode.val){
+      return this.search(val, currentNode.left)
+    } else {
+      return this.search(val, currentNode.right)
+    }
   }
+
 
 
   preOrderTraversal(currentNode = this.root) {
